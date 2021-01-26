7c33ca7d7393212996cf360224cd332b96d8bb4a
Clesio Santana <clesiosmatos@gmail.com>
2021-01-26 09:56:54 -0300

    icon

--- resources/views/layouts/1.blade.php
+++ resources/views/layouts/1.blade.php
@@ -6,13 +6,14 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <title>@yield('title') - Bíblia Free</title>
-
+    
+    <link rel="icon" href="{{ URL::asset('/public/favicon.ico') }}" type="image/x-icon"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   </head>
   <body>
     
 <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <p class="h5 my-0 me-md-auto fw-normal">Bíblia Free</p>
+  <p class="h5 my-0 me-md-auto fw-normal">Bíblia Free </p>
   <nav class="my-2 my-md-0 me-md-3">
     <a class="p-2 text-dark" href="/">Home</a>
     <a class="p-2 text-dark" href="/themes">Temas</a>
@@ -25,8 +26,7 @@
   <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
       <div class="col-12 col-md">
-        <img class="mb-2" src="../assets/brand/bootstrap-logo.svg" alt="" width="24" height="19">
-        <small class="d-block mb-3 text-muted">&copy; 2017-2020</small>
+        <small class="d-block mb-3 text-muted">&copy; 2021-2021</small>
       </div>
       <div class="col-6 col-md">
         <h5>Bíblias</h5>
