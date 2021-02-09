b1cc5c8f240c4256624db6f128f98cf3a12f992a
Clesio Santana <clesiosmatos@gmail.com>
2021-02-09 09:46:56 -0300

    share buttons

--- resources/views/verse.blade.php
+++ resources/views/verse.blade.php
@@ -11,6 +11,14 @@
                     {{ $book }} {{ $chapter }}:{{ $verse }}
                 </a>
             </p>
+            <p>
+                <a target="_blank" href="https://api.whatsapp.com/send?text={{ $verseText }}%0A{{ $book }} {{ $chapter }}:{{ $verse }}" class="btn btn-sm btn-success">
+                    <i class="fab fa-whatsapp"></i> Whatsapp
+                </a>
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://bibliafree.com.br//version/{{ $version }}/book/{{ $book_abreviation }}/{{ $chapter }}/{{ $verse }}" class="btn btn-sm btn-primary">
+                    <i class="fab fa-facebook"></i> Facebook
+                </a>
+            </p>
         </div>
     </div>
 @endsection
